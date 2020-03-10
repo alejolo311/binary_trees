a09095e18e918db9145488ad8a80db5c723cd0f0
Alejo López <alejolo311@gmail.com>
2020-03-10 19:21:09 +0000

    the hegiht

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,11 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+
+
+/* Helpers */
+size_t meausure(const binary_tree_t *tree);
+int max(int a, int b);
 
 #endif /* BINARY_T_H */
