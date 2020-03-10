1dba8cc171cf55b68af25e41d9a50a7a6e07b0a0
Alejo López <alejolo311@gmail.com>
2020-03-10 20:03:48 +0000

    check if a tree is full and check if is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,76 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to head of tree
+ * Return: 1 if true 0 if false
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int size, height, isPerfect;
+
+	if (!tree)
+		return (0);
+	size = (int)binary_tree_size(tree);
+	height = binary_tree_height(tree);
+    isPerfect = size == (2 << height) - 1;
+	return (isPerfect);
+}
+/**
+ * binary_tree_size - measure the size
+ * @tree: input 
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    int size;
+	if (tree == NULL)
+		return (0);
+
+    size = 1 + binary_tree_size(tree->right) + binary_tree_size(tree->left);
+
+	return (size);
+}
+
+/**
+ * binary_tree_height - height of a binary tree
+ * @tree: input tree
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (meausure(tree) - 1);
+}
+/**
+ * measure - helper
+ * @tree: input tree
+ * Return: height of tree
+ */
+size_t meausure(const binary_tree_t *tree)
+{
+	size_t left, right;
+    int total;
+
+	if (tree == NULL)
+		return (0);
+
+	left = meausure(tree->left);
+    right = meausure(tree->right);
+    total = max(left, right) + 1;
+	return (total);
+}
+
+/**
+ * max - helper
+ * @a: the first number
+ * @b: the second number
+ * Return: height of tree
+ */
+int max(int a, int b)
+{
+    if (a > b)
+        return (a);
+    else
+        return (b);
+}
