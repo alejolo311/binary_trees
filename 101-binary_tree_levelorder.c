516c520653249e83f6879981ec05c890b1fff091
alejolo311 <alejolo311@gmail.com>
2020-03-12 15:21:52 +0000

    check if is complete

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order
- * traversal
- * @tree: pointer to root of tree
- * @func: function to be called on each node
+ * binary_tree_levelorder - level order
+ * @tree: tree
+ * @func: func
+ * Return: void
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
--- binary_trees.h
+++ binary_trees.h
@@ -73,5 +73,6 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size);
 /* Helpers */
 size_t measure(const binary_tree_t *tree);
 int max(int a, int b);
+int check(const binary_tree_t *tree, unsigned int index, size_t size);
 
 #endif /* BINARY_T_H */
